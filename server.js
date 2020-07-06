a3e9a41af16cbd1783befdf8a57f34b46a831bae
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-06 00:04:16 -0400

    Updated server with env port variable, Initiated package file with start script and dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const server = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT);
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
