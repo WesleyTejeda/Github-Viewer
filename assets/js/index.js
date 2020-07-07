1183cfea0e2272f931f224bd77cee054aff0ea0d
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-07 00:02:00 -0400

    Updated all files. Stage 2 complete.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,78 @@
 const $userField = $("#inputUser");
 const $searchBtn = $("#search");
-const $formCont = $("#formContainer");
+const $main = $("#main");
 let userName = "";
 let queryURL = "https://api.github.com/users/";
-
-// function getUser(){
-//     return $.ajax({
-//         url: "/"+userName,
-//         method: "GET",
-//     })
-// }
 
 function searchUser(){
     event.preventDefault();
     userName = $userField.val();
-    getGithub();
+    let genQuery = queryURL + userName; 
+    getGithub(genQuery);
 }
 
-function getGithub() {
+function getGithub(query) {
     $.ajax({
-        url: queryURL + userName,
+        url: query,
         method: "GET"
-    }).then( (data) => {
-        postUserInfo(data);
+    }).then( (data1) => {
+        postUserInfo(data1);
     });
 }
-function postUserInfo(data){
-    $formCont.empty();
 
-    let newContent = $("<div>");
-    let gitObj ={
-        gitUser: data.name,
-        gitLoc: data.location,
-        gitProfPic: data.avatar_url,
-        gitURL: data.html_url,
-        gitRepos: data.public_repos,
-    }
-    let image = $("<img>").attr("src", gitObj.gitProfPic);
-    image.attr("style", "width: 200px");
-    let user = $("<p>").html("User: "+gitObj.gitUser);
-    let loc = $("<p>").html("Location: "+gitObj.gitLoc);
-    let url = $("<a>").attr("href", gitObj.gitURL).attr( "style", "color: white").html("Link: "+gitObj.gitURL);
-    let repos = $("<p>").html("Public Respositories: "+gitObj.gitRepos);
-    newContent.append(user);
-    newContent.append(image);
-    newContent.append(loc);
-    newContent.append(url);
-    newContent.append(repos);
-    newContent.attr("style", "color: white");
-    $formCont.append(newContent);
+async function postUserInfo(data1){
+    $main.empty();
+    let followers = 0;
+    let repoHtml =``;
+    $.get(queryURL + userName+"/followers").then(resp => {
+        resp.forEach(() => followers++);
+        console.log(followers);
+        $.get(queryURL + userName+"/repos", data1).then(data2 => {
+            let uniqueId = 0;
+            data2.forEach(data2 => {
+                repoHtml += `
+                <div class="card-body col-2">
+                    <i class="far fa-file-code"></i>
+                    <p class="text-white" id="repo${uniqueId}">${data2.name}</p>
+                    <a href="${data2.url}" id="repo-link${uniqueId}">Repository</a>
+                    <p class="text-white" id="repo-size${uniqueId}">${data2.size} KB</p>
+                </div>`;
+                uniqueId++;
+            });
+    
+            let html =`<section class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="row">
+                        <div id="photoContainer" class="justify-content-between">
+                            <img id="photo" src="${data1.avatar_url}">
+                        </div>
+                    </div>
+                    <div class="row text-center">
+                        <div id="accInfo">
+                            <h2 id="name">${data1.name}</h2>
+                            <p id="location">${data1.location}</p>
+                            <a href="${data1.html_url}" id="url" target="_blank">@${data1.login}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col p-0">
+                    <div class="row m-0 text-center justify-content-center">
+                        <p class="card-body text-white">Public Repositories: ${data1.public_repos}</p>
+                        <p class="card-body text-white">Total Followers: ${followers}</p>
+                    </div>
+                </div>
+            </div>
+            <div id="appendRepos" class="row justify-content-between">
+                ${repoHtml}
+            </div>
+            </section>`;
+            
+            $main.html(html);
+        })
+    });
 }
 
 $searchBtn.on("click", searchUser);
--- server.js
+++ server.js
@@ -12,6 +12,11 @@ server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+"/assets/index.html"));
 })
 
+// server.get("/user/:userName", (req, res) => {
+//     //return template literal html 
+//     res.status(200).send(req.params);
+// });
+
 server.get("*", (req, res) =>{
     res.status(404).send("404: Page Not Found");
 })
