05d97822b689b5f3525084b865c01193b121b050
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-08 00:50:46 -0400

    Added path to retrieve github colors for languages, served back to json in ajax call

--- server.js
+++ server.js
@@ -12,10 +12,9 @@ server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname+"/assets/index.html"));
 })
 
-// server.get("/user/:userName", (req, res) => {
-//     //return template literal html 
-//     res.status(200).send(req.params);
-// });
+server.get("/colors", (req, res) => {
+    res.sendFile(path.join(__dirname+"/json/colors.json"));
+});
 
 server.get("*", (req, res) =>{
     res.status(404).send("404: Page Not Found");
